0e3d5a8479cd79da5f5beb29f4961a0f22cef009
Ricardo Nacif <nacif.ricardo@gmail.com>
2018-05-14 13:56:25 -0300

    with real images now

--- app.py
+++ app.py
@@ -3,17 +3,23 @@ import numpy as np
 from matplotlib import pyplot as plt
 import glob, os
 
-def apply_threshold(imgpath):
+def get_health_numbers(imgpath):
   img = cv2.imread(imgpath, 0)
+  imgcolor = cv2.imread(imgpath, 1)
+  debug_image = imgcolor.copy()
+  
   blur = cv2.GaussianBlur(img,(5,5),0)
   ret3,threshold = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-
+  
+  threshold = cv2.erode(threshold, np.ones((2,2)))
+  # cv2.imshow("T",threshold)
+  # cv2.waitKey(0)
+  # cv2.destroyAllWindows()
 
   
   count, labels, stats, centroids = cv2.connectedComponentsWithStats(threshold)
-  print(stats)
-
-  debug_image = img.copy()
+  # print("x, y, w, h, a")
+  # print(stats)
 
   templates = []
   for p in sorted(glob.glob('./number_templates/*.png')):
@@ -26,11 +32,11 @@ def apply_threshold(imgpath):
         continue
     s = None
     isHealthTotal=False
-    if a > 400 and w > 7 and h > 50:
-        s = 0.4
-    elif a > 100 and w > 3 and h > 25:
+    if a > 200 and w > 5 and h > 38:
+        s = 0.3
+    elif a > 80 and w >= 1 and h >= 19:
         isHealthTotal=True
-        s = 0.20
+        s = 0.15
     else:
         cv2.rectangle(debug_image, (x,y), (x+w, y+h), (0, 0, 255))
         
@@ -39,10 +45,13 @@ def apply_threshold(imgpath):
         region = cv2.copyMakeBorder(region, 10, 10, 10, 30, cv2.BORDER_CONSTANT, 0)
         digit_match = []
         for i, t in enumerate(templates):
+            # cv2.imshow("T",t)
+            
             t = cv2.resize(t, (0, 0), fx=s, fy=s)
-            cv2.imshow('t', t)
-            cv2.imshow('region', region)
-            cv2.waitKey(0)
+            # cv2.imshow("T2",t)
+            # cv2.imshow("region",region)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
             digit_match.append(np.min(cv2.matchTemplate(region, t, cv2.TM_SQDIFF_NORMED)))
             
         digit = np.argmin(digit_match)
@@ -59,10 +68,11 @@ def apply_threshold(imgpath):
         # else:
         #     # raise ValueError('Could not classify digit %d' % (i, ))
 
-  cv2.imshow("debug",debug_image)
-  cv2.waitKey(0)
-  cv2.destroyAllWindows()
+  # cv2.imshow("debug",debug_image)
+  # cv2.waitKey(0)
+  # cv2.destroyAllWindows()
   # print(healthDigits)
+  # print(totalHealthDigits)
   if healthDigits and totalHealthDigits:
     print("----")
     print(imgpath)
@@ -70,30 +80,20 @@ def apply_threshold(imgpath):
     print("total health:" + (''.join(str(d) for d in totalHealthDigits)))
 
 
-# for p in sorted(glob.glob('./test_images/*.png')):
-#       apply_threshold(p)
 
 
-apply_threshold('test_images/3.png')
-# apply_threshold('test_images/8.png', 1)
+
+for p in sorted(glob.glob('./real_images/*.png')):
+      get_health_numbers(p)
+
+
+# get_health_numbers('test_images/3.png')
+# get_health_numbers('real_images/368.png')
+# get_health_numbers('real_images/369.png')
+# get_health_numbers('real_images/380.png')
+# get_health_numbers('real_images/381.png')
+# get_health_numbers('real_images/69.png')
+# get_health_numbers('real_images/70.png')
+# get_health_numbers('test_images/8.png', 1)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
-
-# def rename(dir, pattern):
-#     i = 0
-#     for pathAndFilename in glob.iglob(os.path.join(dir, pattern)):
-#         title, ext = os.path.splitext(os.path.basename(pathAndFilename))
-#         os.rename(pathAndFilename, os.path.join(dir, str(i) + '.png'))
-#         i = i+1
-
-
-
-# rename(r'test_images', r'*.png')
