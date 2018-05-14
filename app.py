b2d19a418383a16ad74356fda85519ec0d66b4a1
Ricardo Nacif <nacif.ricardo@gmail.com>
2018-05-14 00:50:52 -0300

    omg

--- app.py
+++ app.py
@@ -12,36 +12,47 @@ def apply_threshold(imgpath, index):
   
   count, labels, stats, centroids = cv2.connectedComponentsWithStats(threshold)
   print(stats)
-  # cv2.imshow('threshold' + str(index), threshold)
-  # for x, y, w, h, a in stats[1:]:
-  #   # if a > 90:
-  #     region = threshold[y:y+h, x:x+w]
-  #     plt.imshow(region, interpolation='none')
-  #     # cv2.imwrite('result'+ str(x) + ".png", region)
-  #     plt.show()
+
+  debug_image = img.copy()
+
   templates = []
   for p in sorted(glob.glob('./number_templates/*.png')):
       templates.append(cv2.imread(p, 0))
 
   digits = []
   for i, (x, y, w, h, a) in sorted(list(enumerate(stats)), key=lambda e: e[1][0]):
-      if i == 0:
-          continue
-      if a > 150 and w > 10 and h > 50:
-          region = ((labels == i)[y:y+h, x:x+w] * 255).astype(np.uint8)
-          region = cv2.copyMakeBorder(region, 10, 10, 10, 30, cv2.BORDER_CONSTANT, 0)
-          digit_match = []
-          for i, t in enumerate(templates):
-              t = cv2.resize(t, (0, 0), fx=0.4, fy=0.4)
-              digit_match.append(np.min(cv2.matchTemplate(region, t, cv2.TM_SQDIFF_NORMED)))
-              
-          digit = np.argmin(digit_match)
-          if digit_match[digit] < 0.4:
-              digits.append(digit)
-          else:
-              raise ValueError('Could not classify digit %d' % (i, ))
+    if i == 0:
+        continue
+    s = None
+    if a > 500 and w > 10 and h > 50:
+        s = 0.4
+    elif a > 100 and w > 3 and h > 25:
+        s = 0.20
+    else:
+        cv2.rectangle(debug_image, (x,y), (x+w, y+h), (0, 0, 255))
+        
+    if s:
+        region = ((labels == i)[y:y+h, x:x+w] * 255).astype(np.uint8)
+        region = cv2.copyMakeBorder(region, 10, 10, 10, 30, cv2.BORDER_CONSTANT, 0)
+        digit_match = []
+        for i, t in enumerate(templates):
+            t = cv2.resize(t, (0, 0), fx=s, fy=s)
+            digit_match.append(np.min(cv2.matchTemplate(region, t, cv2.TM_SQDIFF_NORMED)))
+            
+        digit = np.argmin(digit_match)
+        
+        cv2.putText(debug_image, '%d' % (digit), (x, y-22), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0))
+        cv2.putText(debug_image, '%1.2f' % (digit_match[digit]), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0))
+        cv2.rectangle(debug_image, (x,y), (x+w, y+h), (0, 255, 0))
+        
+        if digit_match[digit] < 0.4:
+            digits.append(digit)
+        else:
+            raise ValueError('Could not classify digit %d' % (i, ))
 
+  # cv2.imshow("debug",debug_image)
   print(''.join(str(d) for d in digits))
+
 
 
 
